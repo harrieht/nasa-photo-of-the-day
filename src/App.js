c36e44294da6725b785d8cd4131587df0e55a71a
Mary Gabon <marygabon21@gmail.com>
2023-04-12 21:58:01 -0700

    updated MVP

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
 import "./App.css";
 
+import NasaPhoto from "./Components/NasaPhoto";
+
+const dummyData = {
+  date:"2023-04-13",
+  explanation: "Stars of the globular cluster NGC 2419 are packed into this Hubble Space Telescope field of view toward the mostly stealthy constellation Lynx. The two brighter spiky stars near the edge of the frame are within our own galaxy. NGC 2419 itself is remote though, some 300,000 light-years away. In comparison, the Milky Way's satellite galaxy, the Large Magellanic Cloud, is only about 160,000 light-years distant. Roughly similar to other large globular star clusters like Omega Centauri, NGC 2419 is intrinsically bright, but appears faint because it is so far away. Its extreme distance makes it difficult to study and compare its properties with other globular clusters that roam the halo of our Milky Way galaxy. Sometimes called \"the Intergalactic Wanderer\", NGC 2419 really does seem to have come from beyond the Milky Way. Measurements of the cluster's motion through space suggest it once belonged to the Sagittarius dwarf spheroidal galaxy, another small satellite galaxy being disrupted by repeated encounters with the much larger Milky Way.",
+  hdurl: "https://apod.nasa.gov/apod/image/2304/hubble_ngc2419_potw1908a.jpg",
+  title: "NGC 2419: Intergalactic Wanderer"
+}
+
 function App() {
+  const [data, setData] = useState(dummyData);
+ 
+  useState(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(res => {
+      setData(res.data);
+    })
+    .catch(err => console.error(err))
+  }, [])
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      { data && <NasaPhoto photo={data} /> }
     </div>
   );
 }
 
 export default App;
+
